122aa07b4583c396d8b8098958a3dee10e255b8e
Rex Garland <rex@rexgarland.dev>
2023-10-07 20:53:57 -0700

    Organize code for reading

--- notes.sh
+++ notes.sh
@@ -18,14 +18,26 @@ args:
 environment variables:
 - EDITOR: the editor to use (e.g. vim)
 - NOTES_DIR (optional): defaults to ~/.note-store
+
+CAVEATS:
+- If your editor creates swap files to store temporary changes (e.g. for auto-backup), this will leak sensitive note data outside of the protection of this tool. Make sure to disable swap files for your $EDITOR.
 '
 
 NOTES_DIR=${NOTES_DIR:-~/.note-store}
+
+###################### utils ######################
 
 name_to_file() {
 	local name=$1
 	echo $NOTES_DIR/$name.md
 }
+
+get_gpg_id() {
+	local gpgfile=$NOTES_DIR/.gpg-id
+	cat $gpgfile | tr -d "\n"
+}
+
+#################### checkers #####################
 
 check_for_init() {
 	if [[ ! -d $NOTES_DIR ]]; then
@@ -39,11 +51,31 @@ check_for_init() {
 	fi
 }
 
-delete_note() {
+git_initialized() {
+	if [[ ! -d $NOTES_DIR/.git ]]; then
+		return 1
+	fi
+
+	if [[ ! -f $NOTES_DIR/.gitattributes ]]; then
+		return 1
+	fi
+
+	return 0
+}
+
+check_for_git_init() {
+	if ! git_initialized; then
+		echo "Git repo not initialized for notes. Run 'notes git init' first."
+		exit 1
+	fi
+}
+
+check_valid_name() {
 	local name=$1
-	local file=$(name_to_file $name)
-	local gpgfile=$file.gpg
-	rm $gpgfile
+	if [[ -z $name ]]; then
+		echo "Please specify the note's name."
+		exit 1
+	fi
 }
 
 note_exists() {
@@ -60,17 +92,21 @@ note_exists() {
 
 check_note_exists() {
 	local name=$1
-
 	if ! note_exists $name; then
 		echo "Could not find note."
 		exit 1
 	fi
 }
 
-get_gpg_id() {
-	local gpgfile=$NOTES_DIR/.gpg-id
-	cat $gpgfile | tr -d "\n"
+check_note_does_not_exist() {
+	local name=$1
+	if note_exists $name; then
+		echo "Note already exists"
+		exit 1
+	fi
 }
+
+################### encryption ####################
 
 encrypt() {
 	local file=$1
@@ -88,6 +124,48 @@ encrypt() {
 decrypt() {
 	local file=$1
 	gpg --decrypt --quiet $file
+}
+
+##################### actions #####################
+
+init_with_gpg_id() {
+	local gpgid=$1
+	mkdir -p $NOTES_DIR
+	echo $gpgid > $NOTES_DIR/.gpg-id
+}
+
+initialize_git() {
+	cd $NOTES_DIR
+	git init
+	echo "*.gpg diff=gpg" >>.gitattributes
+	cd - >/dev/null
+}
+
+list_notes() {
+	tree -C --noreport $NOTES_DIR | tail -n +2 | sed 's#.md.gpg##' | cat <(echo "Notes store:") -
+}
+
+print_note() {
+	local name=$1
+	local file=$(name_to_file $name)
+	local gpgfile=$file.gpg
+	decrypt $gpgfile
+}
+
+add_note() {
+	local name=$1
+	local file=$(name_to_file $name)
+	local folder=$(dirname $file)
+
+	mkdir -p $folder
+	echo -n "" > $file
+	if ! encrypt $file
+	then
+		rm $file
+		exit 1
+	fi
+	
+	edit_note $name
 }
 
 edit_note() {
@@ -125,83 +203,13 @@ edit_note() {
 	rm $file
 }
 
-add_note() {
-	local name=$1
-	local file=$(name_to_file $name)
-	local folder=$(dirname $file)
-
-	mkdir -p $folder
-	echo -n "" > $file
-	if ! encrypt $file
-	then
-		rm $file
-		exit 1
-	fi
-	
-	edit_note $name
-}
-
-list_notes() {
-	tree -C --noreport $NOTES_DIR | tail -n +2 | sed 's#.md.gpg##' | cat <(echo "Notes store:") -
-}
-
-check_valid_name() {
-	local name=$1
-	if [[ -z $name ]]; then
-		echo "Please specify the note's name."
-		exit 1
-	fi
-}
-
-print_note() {
+delete_note() {
 	local name=$1
 	local file=$(name_to_file $name)
 	local gpgfile=$file.gpg
-	decrypt $gpgfile
+	rm $gpgfile
 }
 
-check_note_does_not_exist() {
-	local name=$1
-	local file=$(name_to_file $name)
-	local gpgfile=$file.gpg
-
-	if [[ -f $gpgfile ]]; then
-		echo "Note already exists"
-		exit 1
-	fi
-}
-
-init_with_gpg_id() {
-	local gpgid=$1
-	mkdir -p $NOTES_DIR
-	echo $gpgid > $NOTES_DIR/.gpg-id
-}
-
-git_initialized() {
-	if [[ ! -d $NOTES_DIR/.git ]]; then
-		return 1
-	fi
-
-	if [[ ! -f $NOTES_DIR/.gitattributes ]]; then
-		return 1
-	fi
-
-	return 0
-}
-
-check_for_git_init() {
-	if ! git_initialized; then
-		echo "Git repo not initialized for notes. Run 'notes git init' first."
-		exit 1
-	fi
-}
-
-initialize_git() {
-	cd $NOTES_DIR
-	git init
-	echo "*.gpg diff=gpg" >>.gitattributes
-	cd - >/dev/null
-}
 
 run_git_command() {
 	cd $NOTES_DIR
@@ -225,6 +233,8 @@ main_git() {
 		run_git_command $@
 	fi
 }
+
+###################### main #######################
 
 main() {
 	local cmd=$1
